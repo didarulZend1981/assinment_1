57ac9ddb4803ade922153540bf94e3fd66d18f87
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-07-08 16:57:36 +0600

    assinment compelete

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,6 @@
+<?php 
+
+$celsius = 32;
+$fahrenheit = ($celsius * 9/5) + 32; // 0°C × 9/5) + 32 = 32°F
+
+printf("The temperature in Fahrenheit is : %.2f Degrees.", $fahrenheit);
---
+++ largest_number.php
@@ -0,0 +1,20 @@
+<?php 
+
+ $num1 = 4; 
+ $num2 = 5;  
+ $num3 = 6;
+
+
+
+
+
+
+if($num1>$num2){
+ 
+  echo "The largest number is: " . $num1;
+}else if($num2>$num3){
+  echo "The largest number is: " . $num2;
+}else{
+  echo "The largest number is: " . $num3;
+}
+
